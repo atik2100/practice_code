650b494290351abe6f208691504213f6f6b886ff
Atik Shahriar <atikbd340@gmail.com>
2024-10-30 22:42:46 +0600

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,142 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class myApp extends StatelessWidget {
+  const myApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: true,
+      title: 'my practice app',
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home'),
+        backgroundColor: Colors.lightGreen,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            SizedBox(
+              height: 50,
+              width: 50,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800,
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                shadowColor: Colors.blueGrey,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(color: Colors.white60, width: 1),
+                ),
+              ),
+              onPressed: () {
+                showDialog(
+                    barrierColor: Colors.brown,
+                    barrierDismissible: true,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Delete'),
+                        backgroundColor: Color(0xFFA7DD9B),
+                        content: Text('Are you sure'),
+                        actions: [
+                          TextButton(onPressed: () {}, child: Text('Yes')),
+                          TextButton(onPressed: () {}, child: Text('No')),
+                        ],
+                      );
+                    });
+              },
+              child: Text('Tap here'),
+            ),
+            SizedBox(height: 32),
+            TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.blue,
+                  textStyle: TextStyle(
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 2,
+                  ),
+                ),
+                onPressed: () {
+                  print('tapped text button');
+                },
+                child: Text('Text button')),
+            SizedBox(height: 32),
+            IconButton(
+              style: IconButton.styleFrom(),
+              onPressed: () {
+                print('tapped icon button');
+              },
+              icon: Icon(Icons.add),
+            ),
+            SizedBox(height: 32),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(),
+              onPressed: () {
+                print('tapped outline button');
+              },
+              child: Text('Outline Button'),
+            ),
+            SizedBox(height: 32),
+            GestureDetector(
+              onTap: () {
+                print('one tap');
+              },
+              onDoubleTap: () {
+                print('double tap');
+              },
+              onLongPress: () {
+                print('long press');
+              },
+              onLongPressEnd: (Details) {
+                print('long press end');
+              },
+              onHorizontalDragDown: (Details) {
+                print('horizontal');
+              },
+              child: Column(
+                children: [
+                  Text('A simple Text'),
+                ],
+              ),
+            ),
+            InkWell(
+              splashColor: Colors.blue,
+              onTap: () {
+                print('ink well');
+              },
+              child: Text('A Ink Well Text'),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          print('tapped fab button');
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
