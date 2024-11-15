84a845f1fcbc2e452b525d67c4093745e60a18c3
Atik Shahriar <atikbd340@gmail.com>
2024-11-15 20:11:27 +0600

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -18,107 +18,136 @@ class myApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  Home({super.key});
+  const Home({super.key});
 
-  List<String> friendList = [
-    'Atik',
-    'Shahriar',
-    'Atia',
-    'Sanzida',
-    'Farjana',
-    'Sharmin',
-    'Kanta',
-    'Abdullah',
-    'Al',
-    'Sakil',
-    'Atik',
-    'Shahriar',
-    'Atia',
-    'Sanzida',
-    'Farjana',
-    'Sharmin',
-    'Kanta',
-    'Abdullah',
-    'Al',
-    'Sakil'
-  ];
-  TextEditingController _EmailTEController = TextEditingController();
-  TextEditingController _PasswordTEController = TextEditingController();
-  GlobalKey<FormState> _fromkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Profile(name: 'Atik',)),
+                );
+              },
+              child: Text('Go To Profile'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              },
+              child: Text('Go To Settings'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class Profile extends StatelessWidget {
+  const Profile({super.key, required this.name});
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(name),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                    (predicate) => false);
+              },
+              child: Text('Go To Home'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              },
+              child: Text('Go To Settings'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class Settings extends StatelessWidget {
+  const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.lightGreen,
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-        ],
+        title: Text('Settings'),
+        backgroundColor: Colors.green,
       ),
-      /*body: ListView.separated(
-          itemCount: friendList.length,
-        itemBuilder: (context, index){
-            return Column(
-              children: [
-                Text(friendList[index]),
-              ],
-            );
-        },
-
-        separatorBuilder: (context, index){
-            return Divider(
-              height: 30,
-            );
-        },
-      ),*/
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.purple,
-                margin: EdgeInsets.all(24),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.all(24),
-                decoration: BoxDecoration(
-                    // borderRadius:BorderRadius.circular(16),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(42),
-                      bottomRight: Radius.circular(42),
-                    ),
-                    color: Colors.purple,
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ]),
-              ),
-            ],
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.all(24),
-            decoration: BoxDecoration(
-              color: Colors.purple,
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image: AssetImage('assets/images/insta.png'),
-              ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                    (predicate) => false);
+              },
+              child: Text('Go to Home'),
             ),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Profile(name: 'Shahriar'),
+                  ),
+                );
+              },
+              child: Text('Go to Profile'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go Back'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+
